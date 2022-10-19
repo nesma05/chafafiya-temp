b285cabc03fb1349ba8c9aa648e9150f78fdd809
Yahia <chalinfy@gmail.com>
2022-10-19 16:54:55 +0000

    fix subtitle after position

--- components/home/SubTitle.tsx
+++ components/home/SubTitle.tsx
@@ -1,6 +1,6 @@
 const SubTitle = () => {
   return (
-    <div className="w-[80%] mx-auto mt-20 mb-10 text-main relative after:absolute after:right-0 top-[50%] after:content-[''] after:h-[8px] after:w-[20%] after:bg-secondary">
+    <div className="w-[80%] mx-auto mt-20 mb-10 text-main relative after:absolute top-[50%] after:content-[''] after:h-[8px] after:w-[20%] after:bg-secondary">
       <h2 className="text-2xl w-[400px] font-bold mb-2">قم بالبحث وحدد ما إذا كنت بحاجة إلى تقديم طلب</h2>
     </div>
   )
