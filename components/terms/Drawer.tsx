fab7652a288221afd14d48c28048af27d2638f0d
Yahia <chalinfy@gmail.com>
2022-10-20 19:00:15 +0000

    edit drawer

--- components/terms/Drawer.tsx
+++ components/terms/Drawer.tsx
@@ -57,7 +57,7 @@ const Drawer = () => {
       <div className="fixed bottom-0 z-40 w-screen sm:w-fit sm:right-4 ">
         <button
           onClick={() => setShowDrawer(true)}
-          className=" w-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-fit sm:rounded-t-lg "
+          className=" w-full bg-main px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-fit sm:rounded-t-lg "
         >
           قاموس/مصطلحات
         </button>
--- components/terms/Terms.tsx
+++ components/terms/Terms.tsx
@@ -5,9 +5,9 @@ const Terms = ({ letter, termsList }: any) => {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className="w-[100%] mb-2 p-1 bg-white">
+    <div className="w-[100%] mb-2 p-1 ">
       <div
-        className="cursor-pointer py-1 px-2 flex items-center gap-2 border-2 border-slate-700 font-medium"
+        className="cursor-pointer py-1 px-2 text-white flex items-center gap-2 border font-medium"
         onClick={() => {
           setOpen(!open)
         }}
@@ -16,15 +16,15 @@ const Terms = ({ letter, termsList }: any) => {
         {open ? <span className='m-start'><MinusIcon/></span> :<span className='m-start'><PlusIcon/></span>}
       </div>
       <div
-        className={`overflow-hidden rounded border-2 border-slate-700 p-2 text-sm font-medium transition-all duration-500 ease-in-out ${
+        className={`overflow-hidden rounded bg-white border p-2 text-sm transition-all duration-500 ease-in-out ${
           open ? 'max-h-[1000px]' : 'max-h-0 py-0'
         }`}
       >
         <ul>
           {termsList.map((term: any) => (
             <li key={term.title} className='mb-2'>
-              <p className="mb-1 underline">{term.title}</p>
-              <p className='text-xs text-gray-700'>{term.explanation}</p>
+              <p className="mb-1 underline font-bold">{term.title}</p>
+              <p className='text-xs'>{term.explanation}</p>
             </li>
           ))}
         </ul>
