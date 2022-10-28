62f864ed09a5f65719ceabf9e57722f70dc750ab
Yahia <chalinfy@gmail.com>
2022-10-28 16:21:00 +0000

    fix documents title position

--- pages/documents.tsx
+++ pages/documents.tsx
@@ -4,31 +4,32 @@ import DocumentsSection from '../components/documents/DocumentsSection'
 import Navbar from '../components/Header/Navbar'
 import PageTitle from '../components/PageTitle'
 
-
-const documentsList = [{
-  thumbnail:"/img/document-1.jpg",
-  title:'القانون رقم 08 المتعلق بحماية ا|شخاص الذاتي  ا',
-  description:'نموذج طلب الحصول على اCعلومات نموذج طلب ا,ذن اCسبق الخاص بمعالجة اCعطيات ذات الطابع الشخصي واCتعلق بتدبير طلبات الحصول على اCعلومات من طرف اCؤسسات والهيئات الخاضعة للقانون 13.31 اCتعلق بالحق في الحصول على اCعلومات',
-  url:'/'
-},
-{
-  thumbnail:"/img/document-2.png",
-  title:'نموذج طلب اليذن المسبق الخاص بمعالجة المعطيات ذات الطابع الشخصي والمتعلق بتدبير طلبات الحصول على المعلومات من طرف سسات والهيئات الخاضعة للقانون 13.31 المتعلق بالحق في الحصول على المعلومات',
-  description:'طلب الحصول على اCعلومات نموذج طلب ا,ذن اCسبق الخاص بمعالجة اCعطيات ذات الطابع الشخصي واCتعلق بتدبير طلبات الحصول على اCعلومات من طرف اCؤسسات والهيئات الخاضعة للقانون 13.31 اCتعلق بالحق في الحصول على اCعلومات',
-  url:'/'
-},]
+const documentsList = [
+  {
+    thumbnail: '/img/document-1.jpg',
+    title: 'القانون رقم 08 المتعلق بحماية ا|شخاص الذاتي  ا',
+    description:
+      'نموذج طلب الحصول على اCعلومات نموذج طلب ا,ذن اCسبق الخاص بمعالجة اCعطيات ذات الطابع الشخصي واCتعلق بتدبير طلبات الحصول على اCعلومات من طرف اCؤسسات والهيئات الخاضعة للقانون 13.31 اCتعلق بالحق في الحصول على اCعلومات',
+    url: '/',
+  },
+  {
+    thumbnail: '/img/document-2.png',
+    title:
+      'نموذج طلب اليذن المسبق الخاص بمعالجة المعطيات ذات الطابع الشخصي والمتعلق بتدبير طلبات الحصول على المعلومات من طرف سسات والهيئات الخاضعة للقانون 13.31 المتعلق بالحق في الحصول على المعلومات',
+    description:
+      'طلب الحصول على اCعلومات نموذج طلب ا,ذن اCسبق الخاص بمعالجة اCعطيات ذات الطابع الشخصي واCتعلق بتدبير طلبات الحصول على اCعلومات من طرف اCؤسسات والهيئات الخاضعة للقانون 13.31 اCتعلق بالحق في الحصول على اCعلومات',
+    url: '/',
+  },
+]
 
 const Documents: NextPage = ({ dir }: any) => {
   // const {locale,locales,asPath} = useRouter()
   return (
-    <div className='font-arabic' dir={dir}>
+    <div className="font-arabic" dir={dir}>
       <Navbar />
-
+      <PageTitle>وثائق قانونية وتنظيمية</PageTitle>
       <div className="my-10 mx-auto w-[90%]">
-        <PageTitle>
-          وثائق قانونية وتنظيمية
-        </PageTitle>
-        <DocumentsSection documentsList={documentsList}/>
+        <DocumentsSection documentsList={documentsList} />
       </div>
     </div>
   )
