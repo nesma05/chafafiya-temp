f2e331ff8088ecda20c8cfb66c4e2c926c6d9026
Yahia <chalinfy@gmail.com>
2022-12-19 14:02:54 +0000

    add popover on request page

--- components/request/SectionTitle.tsx
+++ components/request/SectionTitle.tsx
@@ -1,13 +1,32 @@
-import { NoteIcon } from "../icons"
+import { useState } from 'react'
+import { NoteIcon } from '../icons'
 
 function SectionTitle({ title, subTilte }: any) {
+  const [showNote, setShowNote] = useState(false)
   return (
     <div className="basis-1/3">
-      <h2 className="text-xl font-bold">{title}</h2>
-      <div className="flex gap-2 rounded-md bg-secondary/20 p-2 text-xs">
-        <span className="text-lg"><NoteIcon/></span>
-        <p>يشرح النص المرفق وظائف هذا القسم. يشرح النص المرفق وظائف هذا القسم. يشرح
-        النص المرفق وظائف هذا القسم. يشرح النص المرفق وظائف هذا القسم يشرح</p>
+      <div className="flex items-center gap-1">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <span
+          className="text-lg"
+          onMouseEnter={() => setShowNote(true)}
+          onMouseLeave={() => setShowNote(false)}
+        >
+          <NoteIcon />
+        </span>
+      </div>
+      <div className="relative gap-2 rounded-md p-2 text-xs">
+        <p
+          className={`absolute bg-white p-1.5 shadow-md transition-all duration-150 ease-in-out ${
+            showNote
+              ? 'visible -translate-y-2 opacity-100'
+              : 'invisible translate-y-0 opacity-0'
+          }`}
+        >
+          يشرح النص المرفق وظائف هذا القسم. يشرح النص المرفق وظائف هذا القسم.
+          يشرح النص المرفق وظائف هذا القسم. يشرح النص المرفق وظائف هذا القسم
+          يشرح
+        </p>
       </div>
     </div>
   )
